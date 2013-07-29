e7d54bf4e8cbf99ca645e357558366fb59ddac00
Ilyas Salikhov <salikhoff@gmail.com>
2013-07-29 21:59:24 +0400

    fix messages

--- intaro.intarocrm/lang/ru/install/index.php
+++ intaro.intarocrm/lang/ru/install/index.php
@@ -1,11 +1,11 @@
 <?php
-$MESS ['MODULE_NAME'] = 'Intaro CRM';
-$MESS ['MODULE_DESCRIPTION'] = 'Система обработки заказов';
+$MESS ['MODULE_NAME'] = 'IntaroCRM';
+$MESS ['MODULE_DESCRIPTION'] = 'Модуль интеграции с IntaroCRM — аналитической CRM для электронной коммерции';
 $MESS ['MODULE_PARTNER_NAME'] = 'Интаро Софт';
 $MESS ['MODULE_PARTNER_URI'] = 'http://intaro.ru';
 $MESS ['MODULE_INSTALL_TITLE'] = 'Установка модуля';
 $MESS ['MODULE_UNINSTALL_TITLE'] = 'Удаление модуля';
 $MESS ['CANCELED'] = 'Флаг «Отменен»';
-$MESS ['ERR_SALE'] = 'Отсутсвует модуль sale! Дальнейшая установка невозможна';
-$MESS ['ERR_IBLOCK'] = 'Отсутсвует модуль iblock! Дальнейшая установка невозможна';
-$MESS ['ERR_CATALOG'] = 'Отсутсвует модуль catalog! Дальнейшая установка невозможна';
+$MESS ['ERR_SALE'] = 'Отсутствует модуль sale! Дальнейшая установка невозможна.';
+$MESS ['ERR_IBLOCK'] = 'Отсутствует модуль iblock! Дальнейшая установка невозможна.';
+$MESS ['ERR_CATALOG'] = 'Отсутствует модуль catalog! Дальнейшая установка невозможна.';
