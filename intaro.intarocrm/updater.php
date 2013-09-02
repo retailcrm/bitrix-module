8d883e407df95aa0cd88a83d824e7969dc15e43b
Grisha Pomadchin <gr.pomadchin@gmail.com>
2013-09-02 16:03:27 +0400

    v0.4.0

---
+++ intaro.intarocrm/updater.php
@@ -0,0 +1,12 @@
+<?php
+if (file_exists($_SERVER['DOCUMENT_ROOT'] . '/bitrix/php_interface/include/catalog_export/intarocrm_run.php')) {
+    unlink($_SERVER['DOCUMENT_ROOT'] . '/bitrix/php_interface/include/catalog_export/intarocrm_run.php');
+}
+$updater->CopyFiles("install/export/intarocrm_run.php", "php_interface/include/catalog_export/intarocrm_run.php");
+
+if (file_exists($_SERVER['DOCUMENT_ROOT'] . '/bitrix/php_interface/include/catalog_export/intarocrm_setup.php')) {
+    unlink($_SERVER['DOCUMENT_ROOT'] . '/bitrix/php_interface/include/catalog_export/intarocrm_setup.php');
+}
+$updater->CopyFiles("install/export/intarocrm_setup.php", "php_interface/include/catalog_export/intarocrm_setup.php");
+
+COption::SetOptionString('intaro.intarocrm', 'sites_ids', serialize(array()));
