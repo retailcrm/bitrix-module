f29c0f62a404b20f275c59c378041f081a27c53a
Grisha Pomadchin <gr.pomadchin@gmail.com>
2013-09-09 16:49:08 +0400

    fio parsing fix

--- intaro.intarocrm/classes/general/ICrmOrderActions.php
+++ intaro.intarocrm/classes/general/ICrmOrderActions.php
@@ -538,6 +538,7 @@ class ICrmOrderActions
 
         $resOrder = array();
         $resOrderDeliveryAddress = array();
+        $contactNameArr = array();
 
         $rsOrderProps = CSaleOrderPropsValue::GetList(array(), array('ORDER_ID' => $arFields['ID']));
         while ($ar = $rsOrderProps->Fetch()) {
@@ -553,7 +554,7 @@ class ICrmOrderActions
                         $resOrderDeliveryAddress['city'] = self::toJSON($resOrderDeliveryAddress['city']['CITY_NAME_LANG']);
                     }
                     break;
-                case 'FIO': $resOrder['contactName'] = explode(" ", self::toJSON($ar['VALUE']));
+                case 'FIO': $contactNameArr = self::explodeFIO($ar['VALUE']);
                     break;
                 case 'PHONE': $resOrder['phone'] = $ar['VALUE'];
                     break;
@@ -592,10 +593,7 @@ class ICrmOrderActions
         $createdAt = new \DateTime($arFields['DATE_INSERT']);
         $createdAt = $createdAt->format('Y-m-d H:i:s');
 
-        $resOrder = self::clearArr(array(
-            'lastName'        => $resOrder['contactName'][0],
-            'firstName'       => $resOrder['contactName'][1],
-            'patronymic'      => $resOrder['contactName'][2],
+        $resOrder = array(
             'phone'           => $resOrder['phone'],
             'email'           => $resOrder['email'],
             'deliveryCost'    => $arFields['PRICE_DELIVERY'],
@@ -612,7 +610,16 @@ class ICrmOrderActions
             'createdAt'       => $createdAt,
             'deliveryAddress' => $resOrderDeliveryAddress,
             'items'           => $items
-        ));
+        );
+        
+        // parse fio
+        if(count($contactNameArr) == 1) {
+            $resOrder['firstName'] = $contactNameArr[0];
+        } else {
+            $resOrder['lastName'] = $contactNameArr['contactName'][0];
+            $resOrder['firstName'] = $contactNameArr['contactName'][1];
+            $resOrder['patronymic'] = $contactNameArr['contactName'][2];
+        }
         
         if(isset($arParams['optionsSites']) && is_array($arParams['optionsSites'])
                 && in_array($arFields['LID'], $arParams['optionsSites']))
@@ -671,5 +678,18 @@ class ICrmOrderActions
         global $APPLICATION;
 
         return $APPLICATION->ConvertCharset($str, 'utf-8', SITE_CHARSET);
+    }
+
+    public static function expodeFIO($str) {
+        if(!$str)
+            return array();
+
+        $array = expolde(" ", ICrmOrderActions::toJSON($str), 3);
+
+        foreach($array as &$ar)
+            if(!$ar)
+                unset($ar);
+        
+        return $array;
     }
 }
