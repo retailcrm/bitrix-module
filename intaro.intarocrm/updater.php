eddf81659d6c4282634a9b6fe44a84ff0b4fee1c
Grisha Pomadchin <gr.pomadchin@gmail.com>
2013-10-08 17:07:55 +0400

    revert to 494b5a270

--- intaro.intarocrm/updater.php
+++
@@ -1,9 +0,0 @@
-<?php
-$mid = 'intaro.intarocrm';
-$CRM_API_HOST_OPTION = 'api_host';
-
-$api_host = COption::GetOptionString($mid, $CRM_API_HOST_OPTION, 0);
-$api_host = parse_url($api_host);
-if ($api_host['scheme'] != 'https') $api_host['scheme'] = 'https';
-$api_host = $api_host['scheme'] . '://' . $api_host['host'];
-COption::SetOptionString($mid, $CRM_API_HOST_OPTION, $api_host);
