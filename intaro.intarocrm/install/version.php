74eb5f1d395b3ddd50f2b9d5e86d3a68d482c9b9
Grisha Pomadchin <gr.pomadchin@gmail.com>
2014-07-22 15:35:57 +0400

    +fix userCreation without crm id; v1.0.9

--- intaro.intarocrm/install/version.php
+++ intaro.intarocrm/install/version.php
@@ -1,5 +1,5 @@
 <?
 $arModuleVersion = array(
-        "VERSION" => "1.0.8",
-        "VERSION_DATE" => "2014-07-14 10:49:00"
+        "VERSION" => "1.0.9",
+        "VERSION_DATE" => "2014-07-22 15:35:00"
 );
