6624be556e2ca33ba3e9cf55da86e88f13a1f801
Grisha Pomadchin <gr.pomadchin@gmail.com>
2014-09-04 17:50:57 +0400

    v1.0.10

--- intaro.intarocrm/install/version.php
+++ intaro.intarocrm/install/version.php
@@ -1,5 +1,5 @@
 <?
 $arModuleVersion = array(
-        "VERSION" => "1.0.9",
-        "VERSION_DATE" => "2014-07-22 15:35:00"
+        "VERSION" => "1.0.10",
+        "VERSION_DATE" => "2014-08-12 11:00:00"
 );
