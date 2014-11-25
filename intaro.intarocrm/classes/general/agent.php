567ebf1c22f5ee37ea7cb4a4e0517b44d2791961
Grigory Pomadchin <gr.pomadchin@gmail.com>
2014-11-25 15:57:33 +0300

    compatibility fix

--- intaro.intarocrm/classes/general/agent.php
+++ intaro.intarocrm/classes/general/agent.php
@@ -2,4 +2,4 @@
 require_once($_SERVER['DOCUMENT_ROOT'] . '/bitrix/modules/main/include/prolog_before.php');
 if (!CModule::IncludeModule('intaro.intarocrm')) die('retailCRM not installed.');
 
-ICrmOrderActions::orderAgent();
+ICrmOrderActions::notForkedOrderAgent();
