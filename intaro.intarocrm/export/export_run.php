3d85b30c42b5c91f1823b35943490678ed571f38
Евгений Тыщицкий <tishevgeniy@mail.ru>
2015-05-18 17:38:06 +0300

    v.1.1.2

--- intaro.intarocrm/export/export_run.php
+++ intaro.intarocrm/export/export_run.php
@@ -1,83 +1,87 @@
 <?php
-
-ignore_user_abort(true);
-set_time_limit(0);
-
-global $APPLICATION;
-if (!CModule::IncludeModule("iblock"))
-    return;
-if (!CModule::IncludeModule("catalog"))
-    return;
-if (!CModule::IncludeModule("intaro.intarocrm"))
-    return;
-
-$rsSites = CSite::GetList($by, $sort, array('ACTIVE' => 'Y'));
-while ($ar = $rsSites->Fetch()){
-    if($ar['DEF'] == 'Y'){
-        $SERVER_NAME = $ar['SERVER_NAME'];//разделить потом с учетом многосайтовости
-    }
+if (file_exists($_SERVER["DOCUMENT_ROOT"]."/bitrix/php_interface/retailcrm/export_run.php")){
+    require_once($_SERVER["DOCUMENT_ROOT"]."/bitrix/php_interface/retailcrm/export_run.php");
 }
+else{
+    ignore_user_abort(true);
+    set_time_limit(0);
 
-$iblockProperties = Array(
-        "article" => "article",
-        "manufacturer" => "manufacturer",
-        "color" =>"color",
-        "weight" => "weight",
-        "size" => "size",
-        "length" => "length",
-        "width" => "width",
-        "height" => "height",
-    );
-$IBLOCK_PROPERTY_SKU = array();
-$IBLOCK_PROPERTY_UNIT_SKU = array();
-foreach ($iblockProperties as $prop) {
+    global $APPLICATION;
+    if (!CModule::IncludeModule("iblock"))
+        return;
+    if (!CModule::IncludeModule("catalog"))
+        return;
+    if (!CModule::IncludeModule("intaro.intarocrm"))
+        return;
 
-    $skuUnitProps = ('IBLOCK_PROPERTY_UNIT_SKU' . "_" . $prop);
-    $skuUnitProps = $$skuUnitProps;
-    if (is_array($skuUnitProps)) {
-        foreach ($skuUnitProps as $iblock => $val) {
-            $IBLOCK_PROPERTY_UNIT_SKU[$iblock][$prop] = $val;
+    $rsSites = CSite::GetList($by, $sort, array('ACTIVE' => 'Y'));
+    while ($ar = $rsSites->Fetch()){
+        if($ar['DEF'] == 'Y'){
+            $SERVER_NAME = $ar['SERVER_NAME'];//разделить потом с учетом многосайтовости
         }
     }
 
-    $skuProps = ('IBLOCK_PROPERTY_SKU' . "_" . $prop);
-    $skuProps = $$skuProps;
-    if (is_array($skuProps)) {
-        foreach ($skuProps as $iblock => $val) {
-            $IBLOCK_PROPERTY_SKU[$iblock][$prop] = $val;
+    $iblockProperties = Array(
+            "article" => "article",
+            "manufacturer" => "manufacturer",
+            "color" =>"color",
+            "weight" => "weight",
+            "size" => "size",
+            "length" => "length",
+            "width" => "width",
+            "height" => "height",
+        );
+    $IBLOCK_PROPERTY_SKU = array();
+    $IBLOCK_PROPERTY_UNIT_SKU = array();
+    foreach ($iblockProperties as $prop) {
+
+        $skuUnitProps = ('IBLOCK_PROPERTY_UNIT_SKU' . "_" . $prop);
+        $skuUnitProps = $$skuUnitProps;
+        if (is_array($skuUnitProps)) {
+            foreach ($skuUnitProps as $iblock => $val) {
+                $IBLOCK_PROPERTY_UNIT_SKU[$iblock][$prop] = $val;
+            }
+        }
+
+        $skuProps = ('IBLOCK_PROPERTY_SKU' . "_" . $prop);
+        $skuProps = $$skuProps;
+        if (is_array($skuProps)) {
+            foreach ($skuProps as $iblock => $val) {
+                $IBLOCK_PROPERTY_SKU[$iblock][$prop] = $val;
+            }
         }
     }
+    $IBLOCK_PROPERTY_PRODUCT = array();
+    $IBLOCK_PROPERTY_UNIT_PRODUCT = array();
+    foreach ($iblockProperties as $prop) {
+
+        $productUnitProps = "IBLOCK_PROPERTY_UNIT_PRODUCT" . "_" . $prop;
+        $productUnitProps = $$productUnitProps;
+        if (is_array($productUnitProps)) {
+            foreach ($productUnitProps as $iblock => $val) {
+                $IBLOCK_PROPERTY_UNIT_PRODUCT[$iblock][$prop] = $val;
+            }
+        }
+
+        $productProps = "IBLOCK_PROPERTY_PRODUCT" . "_" . $prop;
+        $productProps = $$productProps;
+        if (is_array($productProps)) {
+            foreach ($productProps as $iblock => $val) {
+                $IBLOCK_PROPERTY_PRODUCT[$iblock][$prop] = $val;
+            }
+        }
+    }
+
+    $loader = new ICMLLoader();
+    $loader->profileID = $PROFILE_ID;
+    $loader->iblocks = $IBLOCK_EXPORT;
+    $loader->propertiesSKU = $IBLOCK_PROPERTY_SKU;
+    $loader->propertiesUnitSKU = $IBLOCK_PROPERTY_UNIT_SKU;
+    $loader->propertiesProduct = $IBLOCK_PROPERTY_PRODUCT;
+    $loader->propertiesUnitProduct = $IBLOCK_PROPERTY_UNIT_PRODUCT;
+    $loader->filename = $SETUP_FILE_NAME;
+    $loader->serverName = $SERVER_NAME;
+    $loader->application = $APPLICATION;
+    $loader->loadPurchasePrice = $LOAD_PURCHASE_PRICE == 'Y';
+    $loader->Load();
 }
-$IBLOCK_PROPERTY_PRODUCT = array();
-$IBLOCK_PROPERTY_UNIT_PRODUCT = array();
-foreach ($iblockProperties as $prop) {
-
-    $productUnitProps = "IBLOCK_PROPERTY_UNIT_PRODUCT" . "_" . $prop;
-    $productUnitProps = $$productUnitProps;
-    if (is_array($productUnitProps)) {
-        foreach ($productUnitProps as $iblock => $val) {
-            $IBLOCK_PROPERTY_UNIT_PRODUCT[$iblock][$prop] = $val;
-        }
-    }
-
-    $productProps = "IBLOCK_PROPERTY_PRODUCT" . "_" . $prop;
-    $productProps = $$productProps;
-    if (is_array($productProps)) {
-        foreach ($productProps as $iblock => $val) {
-            $IBLOCK_PROPERTY_PRODUCT[$iblock][$prop] = $val;
-        }
-    }
-}
-
-$loader = new ICMLLoader();
-$loader->profileID = $PROFILE_ID;
-$loader->iblocks = $IBLOCK_EXPORT;
-$loader->propertiesSKU = $IBLOCK_PROPERTY_SKU;
-$loader->propertiesUnitSKU = $IBLOCK_PROPERTY_UNIT_SKU;
-$loader->propertiesProduct = $IBLOCK_PROPERTY_PRODUCT;
-$loader->propertiesUnitProduct = $IBLOCK_PROPERTY_UNIT_PRODUCT;
-$loader->filename = $SETUP_FILE_NAME;
-$loader->serverName = $SERVER_NAME;
-$loader->application = $APPLICATION;
-$loader->loadPurchasePrice = $LOAD_PURCHASE_PRICE == 'Y';
-$loader->Load();
