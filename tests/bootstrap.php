b0e245e8da64e6f61bcb29e888e869475c18d8fa
Akolzin Dmitry <akolzinvs@gmail.com>
2020-03-17 10:21:11 +0300

    update run tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -9,6 +9,8 @@
  * @see      http://help.retailcrm.ru
  */
 
+require_once __DIR__ . '/../vendor/autoload.php';
+
 if (file_exists(__DIR__ . '/../.env')) {
     $dotenv = Dotenv\Dotenv::create(__DIR__ . '/../');
     $dotenv->load();
