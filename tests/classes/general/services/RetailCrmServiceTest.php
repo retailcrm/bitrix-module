db42218e3eeba3e199ccdefb8cbfb805fd438594
Daniel Weiser <sukhorad@retailcrm.ru>
2020-06-26 15:22:59 +0300

    fixies

--- tests/classes/general/services/RetailCrmServiceTest.php
+++ tests/classes/general/services/RetailCrmServiceTest.php
@@ -6,49 +6,45 @@
 class RetailCrmServiceTest extends PHPUnit\Framework\TestCase
 {
     private $paramsExample = array (
-        array (
-            'number' => '5958C',
-            'externalId' => '8',
-            'createdAt' => '2020-06-22 16:47:49',
-            'customer' => array (
-                'externalId' => '3',
-            ),
-            'orderType' => 'eshop-individual',
-            'status' => 'prepayed',
-            'delivery' => array (
-                'cost' => '0',
-                'address' => array (
-                    'text' => 'ул. Первомайская 41',
-                ),
-            ),
-            'contragent' => array (
-                'contragentType' => 'individual',
-            ),
-            'discountManualAmount' => '0',
-            'discountManualPercent' => '0',
-            'items' => array (
-                array (
-                    'externalIds' => array (
-                        array (
-                            'code' => 'bitrix',
-                            'value' => '0_88',
-                        ),
-                    ),
-                    'quantity' => '1',
-                    'offer' => array (
-                        'externalId' => '88',
-                        'xmlId' => '248',
-                    ),
-                    'productName' => 'Agustí Torelló Mata GR Barrica 2011',
-                    'id' => '9072',
-                    'discountManualPercent' => '0',
-                    'discountManualAmount' => '0',
-                    'initialPrice' => '21.25',
-                ),
+        'number' => '5958C',
+        'externalId' => '8',
+        'createdAt' => '2020-06-22 16:47:49',
+        'customer' => array (
+            'externalId' => '3',
+        ),
+        'orderType' => 'eshop-individual',
+        'status' => 'prepayed',
+        'delivery' => array (
+            'cost' => '0',
+            'address' => array (
+                'text' => 'ул. Первомайская 41',
             ),
         ),
-        'externalId',
-        '',
+        'contragent' => array (
+            'contragentType' => 'individual',
+        ),
+        'discountManualAmount' => '0',
+        'discountManualPercent' => '0',
+        'items' => array (
+            array (
+                'externalIds' => array (
+                    array (
+                        'code' => 'bitrix',
+                        'value' => '0_88',
+                    ),
+                ),
+                'quantity' => '1',
+                'offer' => array (
+                    'externalId' => '88',
+                    'xmlId' => '248',
+                ),
+                'productName' => 'Agustí Torelló Mata GR Barrica 2011',
+                'id' => '9072',
+                'discountManualPercent' => '0',
+                'discountManualAmount' => '0',
+                'initialPrice' => '21.25',
+            ),
+        ),
     );
 
     private $errorsExample = array (
@@ -61,9 +57,9 @@ class RetailCrmServiceTest extends PHPUnit\Framework\TestCase
     {
         $newParams = RetailCrmService::unsetIntegrationDeliveryFields($this->paramsExample, $this->errorsExample);
         $expectedArray = $this->paramsExample;
-        unset($expectedArray[0]['firstName']);
-        unset($expectedArray[0]['lastName']);
-        unset($expectedArray[0]['delivery']['address']);
+        unset($expectedArray['firstName']);
+        unset($expectedArray['lastName']);
+        unset($expectedArray['delivery']['address']);
 
         $this->assertEquals($newParams, $expectedArray);
     }
