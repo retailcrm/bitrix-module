f2c8e79116cf86063933d12f3c67730043b03b80
Павел <pashok9825@gmail.com>
2020-07-29 15:22:26 +0300

    fuser repository & correct annotations

---
+++ intaro.retailcrm/lib/model/bitrix/fuser.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * PHP version 7.1
+ *
+ * @category Integration
+ * @package  Intaro\RetailCrm\Model\Bitrix
+ * @author   retailCRM <integration@retailcrm.ru>
+ * @license  MIT
+ * @link     http://retailcrm.ru
+ * @see      http://retailcrm.ru/docs
+ */
+namespace Intaro\RetailCrm\Model\Bitrix;
+
+use Bitrix\Main\Type\DateTime;
+
+/**
+ * Class Fuser
+ *
+ * @package Intaro\RetailCrm\Model\Bitrix
+ *
+ * @method int getId()
+ * @method void setId(int $id)
+ * @method DateTime getDateInsert()
+ * @method void setDateInsert(DateTime $dateInsert)
+ * @method DateTime getDateIns()
+ * @method void setDateIns(DateTime $dateIns)
+ * @method DateTime getDateUpdate()
+ * @method void setDateUpdate(DateTime $dateUpdate)
+ * @method DateTime getDateUpd()
+ * @method void setDateUpd(DateTime $dateUpd)
+ * @method mixed getUser()
+ * @method void setUser($user)
+ */
+class Fuser extends AbstractModelFacade
+{
+}
