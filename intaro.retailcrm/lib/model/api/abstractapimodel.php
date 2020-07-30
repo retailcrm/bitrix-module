4ba68803f04c731b6a4bec8753ec7e3c618c5917
Павел <pashok9825@gmail.com>
2020-07-30 15:15:53 +0300

    AbstractApiModel shoudn't implement Traversable

--- intaro.retailcrm/lib/model/api/abstractapimodel.php
+++ intaro.retailcrm/lib/model/api/abstractapimodel.php
@@ -20,7 +20,7 @@ use Intaro\RetailCrm\Component\Utils;
  *
  * @package Intaro\RetailCrm\Model\Api
  */
-class AbstractApiModel implements ApiModelInterface, \Traversable
+class AbstractApiModel implements ApiModelInterface
 {
     /**
      * @PostDeserialize()
