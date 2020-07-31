f44e9a14239b7d8052b29bd14c206df754f100ce
Павел <pashok9825@gmail.com>
2020-07-31 15:17:54 +0300

    fix DateTimeConverter test compatibility with lower php versions

--- tests/lib/component/converter/DateTimeConverterTest.php
+++ tests/lib/component/converter/DateTimeConverterTest.php
@@ -8,9 +8,17 @@ use Intaro\RetailCrm\Component\Converter\DateTimeConverter;
 
 class DateTimeConverterTest extends TestCase
 {
+    /**
+     * Better rely on preconfigured format & data
+     */
+    const ISO8601 = 'Y-m-d\TH:i:sO';
+
+    /** @var string */
+    const ISO8601_DATE = '1970-01-01T12:30:45+0300';
+
     public function testPhpToBitrix(): void
     {
-        $dateTime = \DateTime::createFromFormat(\DateTime::RFC3339, '1970-01-01T00:01:02');
+        $dateTime = \DateTime::createFromFormat(self::ISO8601, self::ISO8601_DATE);
         $bitrixDateTime = DateTimeConverter::phpToBitrix($dateTime);
 
         self::assertEquals(
@@ -21,7 +29,7 @@ class DateTimeConverterTest extends TestCase
 
     public function testBitrixToPhp(): void
     {
-        $dateTime = \DateTime::createFromFormat(\DateTime::RFC3339, '1970-01-01T00:01:02');
+        $dateTime = \DateTime::createFromFormat(self::ISO8601, self::ISO8601_DATE);
         $bitrixDateTime = DateTime::createFromPhp($dateTime);
         $dateTime = DateTimeConverter::bitrixToPhp($bitrixDateTime);
 
