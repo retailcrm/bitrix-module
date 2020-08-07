ef199c0765fbccb2b995075a09e90e0cb9550f7c
Neur0toxine <pashok9825@gmail.com>
2020-08-07 12:38:53 +0300

    New module structure (refactoring)
    * simple serializer and deserializer with models, new architecture
    * move logic to strategies
    * datetime strategy, some changes in order to make subtype parsing possible
    * fallback to array casting for internal objects
    * serializer now supports typed arrays and formatted DateTime
    * models, prevent hangup from circular serialization of empty entities, improve compatibility with JMS Serializer (to simplify usage), (de)serialization callbacks
    * fuser repository & correct annotations
    * correct naming for proxy
    * move several helper methods to the new structure
    * customizable customer building
    * more models, api corporate customer builder & bitrix customer builder
    * AbstractApiModel shoudn't implement Traversable
    * check if user exists in the order before building corporate customer
    * don't check _rc cookie; it's untestable for now
    * better structure + service locator, cookie extraction should be testable now
    * enable forgotten domaincollector cookie extraction
    * partial api client facade implementation (full implementation is not necessary for now)
    * remove unimportant assert in tests, move utils to service
    * move identifierspair to correct directory
    * move CookieExtractor assert to a single test
    * more models & ClientAdapter
    * fix DateTimeConverter test compatibility with lower php versions
    * update test date format
    * finish customer-related methods in the client facade, fixes in models
    * minor tweaks for (de)serializer, compatibility layer for models which implement types without full functionality
    * make model's getters nullable
    * better logic for AbstractSerializableModel
    * different logic for AbstractSerializableModel
    * ability to create filled AbstractSerializableModel by passing primary key value
    * reuse logic from repositories for passing primary as constructor parameter for AbstractSerializableModel

---
+++ intaro.retailcrm/lib/component/json/strategy/serialize/simpletypestrategy.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * PHP version 7.1
+ *
+ * @category Integration
+ * @package  Intaro\RetailCrm\Component\Json\Strategy\Serialize
+ * @author   retailCRM <integration@retailcrm.ru>
+ * @license  MIT
+ * @link     http://retailcrm.ru
+ * @see      http://retailcrm.ru/docs
+ */
+namespace Intaro\RetailCrm\Component\Json\Strategy\Serialize;
+
+use Intaro\RetailCrm\Component\Json\Strategy\StrategyFactory;
+
+/**
+ * Class SimpleTypeStrategy
+ *
+ * @package Intaro\RetailCrm\Component\Json\Strategy\Serialize
+ */
+class SimpleTypeStrategy implements SerializeStrategyInterface
+{
+    use InnerTypeTrait;
+
+    /**
+     * @inheritDoc
+     */
+    public function serialize($value)
+    {
+        switch (gettype($value)) {
+            case 'bool':
+            case 'boolean':
+                return (bool) $value;
+            case 'int':
+            case 'integer':
+                return (int) $value;
+            case 'float':
+                return (float) $value;
+            case 'double':
+                return (double) $value;
+            case 'string':
+                return (string) $value;
+            default:
+                if (is_iterable($value)) {
+                    $result = [];
+
+                    foreach ($value as $key => $item) {
+                        $result[$key] = StrategyFactory::serializeStrategyByType(gettype($item))->serialize($item);
+                    }
+
+                    return $result;
+                }
+
+                return null;
+        }
+    }
+}
---
+++ intaro.retailcrm/lib/service/utils.php
@@ -0,0 +1,124 @@
+<?php
+/**
+ * PHP version 7.1
+ *
+ * @category Integration
+ * @package  Intaro\RetailCrm\Component
+ * @author   retailCRM <integration@retailcrm.ru>
+ * @license  MIT
+ * @link     http://retailcrm.ru
+ * @see      http://retailcrm.ru/docs
+ */
+namespace Intaro\RetailCrm\Service;
+
+use Bitrix\Main\Text\Encoding;
+
+/**
+ * Class Utils
+ *
+ * @package Intaro\RetailCrm\Component
+ */
+class Utils
+{
+    /**
+     * Removes all empty fields from arrays, works for nested arrays
+     *
+     * @param array $arr
+     * @return array
+     */
+    public function clearArray($arr)
+    {
+        if (is_array($arr) === false) {
+            return $arr;
+        }
+
+        $result = array();
+
+        foreach ($arr as $index => $node) {
+            $result[$index] = is_array($node) === true ? $this->clearArray($node) : trim($node);
+
+            if ($result[$index] == '' || $result[$index] === null || count($result[$index]) < 1) {
+                unset($result[$index]);
+            }
+        }
+
+        return $result;
+    }
+
+    /**
+     *
+     * @param array|bool|\SplFixedArray|string $string in SITE_CHARSET
+     *
+     * @return array|bool|\SplFixedArray|string $str in utf-8
+     */
+    public function toUTF8($string)
+    {
+        if (!defined('SITE_CHARSET')) {
+            throw new \RuntimeException('SITE_CHARSET must be defined.');
+        }
+
+        return $this->convertCharset($string, SITE_CHARSET, 'utf-8');
+    }
+
+    /**
+     *
+     * @param string|array|\SplFixedArray $string in utf-8
+     *
+     * @return array|bool|\SplFixedArray|string $str in SITE_CHARSET
+     */
+    public function fromUTF8($string)
+    {
+        if (!defined('SITE_CHARSET')) {
+            throw new \RuntimeException('SITE_CHARSET must be defined.');
+        }
+
+        return $this->convertCharset($string, 'utf-8', SITE_CHARSET);
+    }
+
+    /**
+     * Returns true if provided PERSON_TYPE_ID is corporate customer
+     *
+     * @param string $personTypeId
+     *
+     * @return bool
+     */
+    public function isPersonCorporate(string $personTypeId): bool
+    {
+        return ConfigProvider::getContragentTypeForPersonType($personTypeId) === Constants::CORPORATE_CONTRAGENT_TYPE;
+    }
+
+    /**
+     * @return string
+     */
+    public function createPlaceholderEmail(): string
+    {
+        return uniqid('user_' . time(), false) . '@example.com';
+    }
+
+    /**
+     * @return string
+     */
+    public function createPlaceholderPassword(): string
+    {
+        return uniqid("R", false);
+    }
+
+    /**
+     * @param string $string
+     * @param string $inCharset
+     * @param string $outCharset
+     *
+     * @return array|bool|\SplFixedArray|string
+     */
+    protected function convertCharset(string $string, string $inCharset, string $outCharset)
+    {
+        $error = '';
+        $result = Encoding::convertEncoding($string,  $inCharset, $outCharset, $error);
+
+        if (!$result && !empty($error)) {
+            throw new \RuntimeException($error);
+        }
+
+        return $result;
+    }
+}
