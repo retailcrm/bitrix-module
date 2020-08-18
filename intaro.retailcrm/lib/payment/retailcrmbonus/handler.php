a09c1171b2078fc5a07078b350747db7d67c3a94
Sergey Chazov <oitv18@gmail.com>
2020-08-18 18:29:24 +0300

    add custom fields

---
+++ intaro.retailcrm/lib/payment/retailcrmbonus/handler.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Sale\Handlers\PaySystem;
+
+use Bitrix\Main\Localization\Loc;
+use Bitrix\Main\Request;
+use Bitrix\Sale\PaySystem;
+use Bitrix\Sale\Payment;
+
+Loc::loadMessages(__FILE__);
+
+/**
+ * Class RetailCrmBonusHandler
+ * @package Sale\Handlers\PaySystem
+ */
+class RetailCrmBonusHandler extends PaySystem\BaseServiceHandler
+{
+    /**
+     * @param Payment      $payment
+     * @param Request|null $request
+     * @return PaySystem\ServiceResult
+     */
+    public function initiatePay(Payment $payment, Request $request = null)
+    {
+        return new PaySystem\ServiceResult();
+    }
+    
+    /**
+     * @return array
+     */
+    public function getCurrencyList()
+    {
+        return [];
+    }
+    
+}
