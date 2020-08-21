1887129bd8bd588c958a6eb51009be0ac3767338
Sergey Chazov <oitv18@gmail.com>
2020-08-21 14:56:53 +0300

    fix codestyle after codereview

--- intaro.retailcrm/install/index.php
+++ intaro.retailcrm/install/index.php
@@ -1499,6 +1499,7 @@ class intaro_retailcrm extends CModule
         foreach ($persons as $person) {
             $personId = $person->getID();
             $groupID = $this->getGroupID($personId);
+            
             if (isset($groupID)) {
                 $this->addBonusField($personId, $groupID);
             }
