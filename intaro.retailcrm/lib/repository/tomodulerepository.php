92189641cb1acb788d2a070803a2cacbc33c2719
Sergey Chazov <oitv18@gmail.com>
2020-08-21 11:21:53 +0300

    included class files in install.php

--- intaro.retailcrm/lib/repository/tomodulerepository.php
+++ intaro.retailcrm/lib/repository/tomodulerepository.php
@@ -41,12 +41,12 @@ class ToModuleRepository extends AbstractRepository
     /**
      * @param array $select
      * @param array $where
-     * @return \Bitrix\Main\Type\Collection|null
+     * @return \Bitrix\Main\Type\Collection|Intaro\RetailCrm\Model\Bitrix\ORM\EO_ToModule_Collection|null
      * @throws \Bitrix\Main\ArgumentException
      * @throws \Bitrix\Main\ObjectPropertyException
      * @throws \Bitrix\Main\SystemException
      */
-    public static function getCollectionByWhere(array $select, array $where): ?Collection
+    public static function getCollectionByWhere(array $select, array $where)
     {
         return ToModuleTable::query()
             ->setSelect($select)
