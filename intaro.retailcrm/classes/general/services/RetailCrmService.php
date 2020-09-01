5fbaafd4d7b273c506c5e15660c905b5301ecabe
DmitreyZl <70319100+DmitreyZl@users.noreply.github.com>
2020-09-01 16:49:49 +0300

    Update RetailCrmService.php

--- intaro.retailcrm/classes/general/services/RetailCrmService.php
+++ intaro.retailcrm/classes/general/services/RetailCrmService.php
@@ -7,31 +7,41 @@ class RetailCrmService
         $deliveryCode = $order['delivery']['code'];
         if ($deliveryCode) {
             switch ($integrationDelivery[$deliveryCode]) {
-                case "sdek":
+                 case "sdek":
                     unset($order['number']);
                     unset($order['height']);
                     unset($order['length']);
                     unset($order['width']);
+					unset($order['weight']);
+            		unset($order['phone']);
+            		unset($order['delivery']['cost']);
+            		unset($order['shipmentStore']);
+            		unset($order['delivery']['address']);
+            		unset($order['delivery']['data']);
                     break;
                 case "dpd":
                     unset($order['manager']);
                     unset($order['firstName']);
                     unset($order['lastName']);
+					unset($order['weight']);
+            		unset($order['phone']);
+            		unset($order['delivery']['cost']);
+            		unset($order['shipmentStore']);
+            		unset($order['delivery']['address']);
+            		unset($order['delivery']['data']);
                     break;
                 case "newpost":
                     unset($order['customer']);
+					unset($order['weight']);
+            		unset($order['phone']);
+            		unset($order['delivery']['cost']);
+            		unset($order['shipmentStore']);
+            		unset($order['delivery']['address']);
+            		unset($order['delivery']['data']);
                     break;
                 default:
-                    unset($order['firstName']);
-                    unset($order['lastName']);
+					break;
             }
-
-            unset($order['weight']);
-            unset($order['phone']);
-            unset($order['delivery']['cost']);
-            unset($order['shipmentStore']);
-            unset($order['delivery']['address']);
-            unset($order['delivery']['data']);
         }
 
         return $order;
