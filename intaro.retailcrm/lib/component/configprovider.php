5e300e60c43cfa4f5362c5b2e9d66ae0fc6302cb
Sergey Chazov <oitv18@gmail.com>
2020-09-01 17:58:52 +0300

    fix unserialize problem on step 4

--- intaro.retailcrm/lib/component/configprovider.php
+++ intaro.retailcrm/lib/component/configprovider.php
@@ -761,7 +761,7 @@ class ConfigProvider
      */
     protected static function getUnserializedOption($option, $def = 0)
     {
-        return unserialize(static::getOption($option, $def), []);
+        return unserialize(static::getOption($option, $def));
     }
 
     /**
