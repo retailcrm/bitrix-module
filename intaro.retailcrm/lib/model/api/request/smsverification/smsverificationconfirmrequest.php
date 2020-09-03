de7873c3a97cc3fa4b88e95e2d9994e3f1514b12
Сергей Чазов <45812598+Chazovs@users.noreply.github.com>
2020-09-03 16:36:03 +0300

    add factory class (#128)

---
+++ intaro.retailcrm/lib/model/api/request/smsverification/smsverificationconfirmrequest.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * PHP version 7.1
+ *
+ * @category Integration
+ * @package  Intaro\RetailCrm\Model\Api\Request\SmsVerification
+ * @author   retailCRM <integration@retailcrm.ru>
+ * @license  MIT
+ * @link     http://retailcrm.ru
+ * @see      http://retailcrm.ru/docs
+ */
+namespace Intaro\RetailCrm\Model\Api\Request\SmsVerification;
+
+use Intaro\RetailCrm\Component\Json\Mapping;
+use Intaro\RetailCrm\Model\Api\AbstractApiModel;
+
+/**
+ * Class SmsVerificationConfirmRequest
+ *
+ * @package Intaro\RetailCrm\Model\Api\Response\SmsVerification
+ */
+class SmsVerificationConfirmRequest extends AbstractApiModel
+{
+    /**
+     * @var \Intaro\RetailCrm\Model\Api\SmsVerificationConfirm
+     *
+     * @Mapping\Type("\Intaro\RetailCrm\Model\Api\SmsVerificationConfirm")
+     * @Mapping\SerializedName("verification")
+     */
+    public $verification;
+}
---
+++ tests/lib/component/factory/ClientFactoryTest.php
@@ -0,0 +1,22 @@
+<?php
+namespace Tests\Intaro\RetailCrm\Component\Factory;
+
+use Bitrix\Main\Config\Option;
+use Intaro\RetailCrm\Component\ApiClient\ClientAdapter;
+use Intaro\RetailCrm\Component\ConfigProvider;
+use Intaro\RetailCrm\Component\Factory\ClientFactory;
+use PHPUnit\Framework\TestCase;
+
+class ClientFactoryTest extends TestCase
+{
+    public function testCreacteClientAdapter(): void
+    {
+        $client = ClientFactory::creacteClientAdapter();
+    
+        if (empty(ConfigProvider::getApiUrl())) {
+            self::assertEquals(null, $client);
+        } else {
+            self::assertEquals(ClientAdapter::class, get_class($client));
+        }
+    }
+}
