8eae088d476c07b73f9eebc600237ab3718a9ad5
DmitreyZl <70319100+DmitreyZl@users.noreply.github.com>
2020-09-04 15:03:48 +0300

    Update RetailCrmService.php

--- intaro.retailcrm/classes/general/services/RetailCrmService.php
+++ intaro.retailcrm/classes/general/services/RetailCrmService.php
@@ -12,28 +12,37 @@ class RetailCrmService
                     unset($order['height']);
                     unset($order['length']);
                     unset($order['width']);
+                    unset($order['weight']);
+                    unset($order['phone']);
+                    unset($order['delivery']['cost']);
+                    unset($order['shipmentStore']);
+                    unset($order['delivery']['address']);
+                    unset($order['delivery']['data']);
                     break;
                 case "dpd":
                     unset($order['manager']);
                     unset($order['firstName']);
                     unset($order['lastName']);
+                    unset($order['weight']);
+                    unset($order['phone']);
+                    unset($order['delivery']['cost']);
+                    unset($order['shipmentStore']);
+                    unset($order['delivery']['address']);
+                    unset($order['delivery']['data']);
                     break;
                 case "newpost":
                     unset($order['customer']);
+                    unset($order['weight']);
+                    unset($order['phone']);
+                    unset($order['delivery']['cost']);
+                    unset($order['shipmentStore']);
+                    unset($order['delivery']['address']);
+                    unset($order['delivery']['data']);
                     break;
                 default:
-                    unset($order['firstName']);
-                    unset($order['lastName']);
+                    break;
             }
-
-            unset($order['weight']);
-            unset($order['phone']);
-            unset($order['delivery']['cost']);
-            unset($order['shipmentStore']);
-            unset($order['delivery']['address']);
-            unset($order['delivery']['data']);
         }
-
         return $order;
     }
 }
