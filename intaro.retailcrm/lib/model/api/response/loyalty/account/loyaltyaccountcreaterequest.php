fa4cbcb0039ac4ddd77fb830a140902429b73192
Sergey Chazov <oitv18@gmail.com>
2020-09-28 16:26:59 +0300

    add request models

---
+++ intaro.retailcrm/lib/model/api/response/loyalty/account/loyaltyaccountcreaterequest.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * PHP version 7.1
+ *
+ * @category Integration
+ * @package  Intaro\RetailCrm\Model\Api\Request\Loyalty\Account
+ * @author   retailCRM <integration@retailcrm.ru>
+ * @license  MIT
+ * @link     http://retailcrm.ru
+ * @see      http://retailcrm.ru/docs
+ */
+namespace Intaro\RetailCrm\Model\Api\Request\Loyalty\Account;
+
+use Intaro\RetailCrm\Component\Json\Mapping;
+use Intaro\RetailCrm\Model\Api\AbstractApiModel;
+
+/**
+ * Class LoyaltyAccountCreateRequest
+ *
+ * @package Intaro\RetailCrm\Model\Api\Response\SmsVerification
+ */
+class LoyaltyAccountCreateRequest extends AbstractApiModel
+{
+    /**
+     * @var \Intaro\RetailCrm\Model\Api\SerializedCreateLoyaltyAccount
+     *
+     * @Mapping\Type("\Intaro\RetailCrm\Model\Api\SerializedCreateLoyaltyAccount")
+     * @Mapping\SerializedName("loyalty_account")
+     */
+    public $loyaltyAccount;
+}
