222f3fab59477cd73d1566eeeab22440df3f4203
Sergey Chazov <oitv18@gmail.com>
2020-09-29 14:26:50 +0300

    add props for SmsVerificationStatusResponse class

--- intaro.retailcrm/lib/model/api/response/smsverification/smsverificationstatusresponse.php
+++ intaro.retailcrm/lib/model/api/response/smsverification/smsverificationstatusresponse.php
@@ -9,11 +9,11 @@
  * @link     http://retailcrm.ru
  * @see      http://retailcrm.ru/docs
  */
-
 namespace Intaro\RetailCrm\Model\Api\Response\SmsVerification;
 
 use Intaro\RetailCrm\Model\Api\AbstractApiModel;
 use Intaro\RetailCrm\Component\Json\Mapping;
+use Intaro\RetailCrm\Model\Api\SmsVerification;
 
 /**
  * Class SmsVerificationStatusResponse
@@ -22,4 +22,54 @@ use Intaro\RetailCrm\Component\Json\Mapping;
  */
 class SmsVerificationStatusResponse extends SmsVerificationConfirmResponse
 {
+    /**
+     * Результат запроса (успешный/неуспешный)
+ 
+     *
+     * @var boolean $success
+     *
+     * @Mapping\Type("boolean")
+     * @Mapping\SerializedName("success")
+     */
+    private $success;
+    
+    /**
+     * @var \Intaro\RetailCrm\Model\Api\SmsVerification
+     *
+     * @Mapping\Type("\Intaro\RetailCrm\Model\Api\SmsVerification")
+     * @Mapping\SerializedName("verification")
+     */
+    private $verification;
+    
+    /**
+     * @return \Intaro\RetailCrm\Model\Api\SmsVerification
+     */
+    public function getVerification(): SmsVerification
+    {
+        return $this->verification;
+    }
+    
+    /**
+     * @param \Intaro\RetailCrm\Model\Api\SmsVerification $verification
+     */
+    public function setVerification(SmsVerification $verification): void
+    {
+        $this->verification = $verification;
+    }
+    
+    /**
+     * @return bool
+     */
+    public function isSuccess(): bool
+    {
+        return $this->success;
+    }
+    
+    /**
+     * @param bool $success
+     */
+    public function setSuccess(bool $success): void
+    {
+        $this->success = $success;
+    }
 }
