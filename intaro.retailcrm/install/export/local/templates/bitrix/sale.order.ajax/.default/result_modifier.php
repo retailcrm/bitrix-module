6b2d2888877dd5b9c7e9843c3b3d73080ada79cb
Sergey Chazov <oitv18@gmail.com>
2020-09-30 11:26:45 +0300

    add component sale.order.ajax and template

---
+++ intaro.retailcrm/install/export/local/templates/bitrix/sale.order.ajax/.default/result_modifier.php
@@ -0,0 +1,13 @@
+<? use Intaro\RetailCrm\Component\ConfigProvider;
+
+if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
+
+/**
+ * @var array $arParams
+ * @var array $arResult
+ * @var SaleOrderAjax $component
+ */
+
+$arResult['LOYALTY_STATUS'] = ConfigProvider::getLoyaltyProgramStatus();
+$component = $this->__component;
+$component::scaleImages($arResult['JS_DATA'], $arParams['SERVICES_IMAGES_SCALING']);
