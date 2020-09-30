492721af80b4c21016cf92a6c400e8f917566ea8
Sergey Chazov <oitv18@gmail.com>
2020-09-30 11:26:45 +0300

    add component sale.order.ajax and template

---
+++ intaro.retailcrm/install/export/local/components/intaro/main.register/lang/ru/.description.php
@@ -0,0 +1,4 @@
+<?php
+$MESS ['COMP_MAIN_USER_REGISTER_TITLE'] = "Настраиваемая регистрация retailCRM";
+$MESS ['COMP_MAIN_USER_REGISTER_DESCR'] = "Управляемая форма регистрации нового пользователя с функциями Программы лояльности";
+$MESS ['MAIN_USER_GROUP_NAME'] = "Пользователь";
