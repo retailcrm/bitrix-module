7075cb469a1219d7224f919a1090374db8c4f6d8
Sergey Chazov <oitv18@gmail.com>
2020-09-30 11:26:45 +0300

    add component sale.order.ajax and template

---
+++ intaro.retailcrm/install/export/local/components/intaro/sale.order.ajax/lang/ru/.description.php
@@ -0,0 +1,4 @@
+<?
+$MESS ['SOF_DEFAULT_TEMPLATE_NAME1'] = "retailCRM Оформление заказа";
+$MESS ['SOF_DEFAULT_TEMPLATE_DESCRIPTION'] = "Компонент оформления заказа с функцией программой лояльность retailCRM";
+$MESS ['SOF_NAME'] = "Процедура оформления заказа";
