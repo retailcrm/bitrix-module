52d53f1194bf09e9a33750d11d3901e06df51a48
Sergey Chazov <oitv18@gmail.com>
2020-09-30 10:57:08 +0300

    delete setters and getters for DTO models

--- intaro.retailcrm/lib/model/api/loyaltylevel.php
+++ intaro.retailcrm/lib/model/api/loyaltylevel.php
@@ -26,7 +26,7 @@ class LoyaltyLevel
      * @Mapping\Type("string")
      * @Mapping\SerializedName("id")
      */
-    private $id;
+    public $id;
     
     /**
      * Название уровня
@@ -36,37 +36,5 @@ class LoyaltyLevel
      * @Mapping\Type("string")
      * @Mapping\SerializedName("name")
      */
-    private $name;
-    
-    /**
-     * @return int
-     */
-    public function getId(): int
-    {
-        return $this->id;
-    }
-    
-    /**
-     * @param int $id
-     */
-    public function setId(int $id): void
-    {
-        $this->id = $id;
-    }
-    
-    /**
-     * @return string
-     */
-    public function getName(): string
-    {
-        return $this->name;
-    }
-    
-    /**
-     * @param string $name
-     */
-    public function setName(string $name): void
-    {
-        $this->name = $name;
-    }
+    public $name;
 }
