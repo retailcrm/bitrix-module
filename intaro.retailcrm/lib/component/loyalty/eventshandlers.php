f8fb4e888d90a1fd2bccdbc4dcfc4250ef1f2a10
Sergey Chazov <oitv18@gmail.com>
2020-10-01 13:33:01 +0300

    add in total price block bonus count

--- intaro.retailcrm/lib/component/loyalty/eventshandlers.php
+++ intaro.retailcrm/lib/component/loyalty/eventshandlers.php
@@ -31,7 +31,7 @@ use Intaro\RetailCrm\Service\LoyaltyService;
 class EventsHandlers
 {
     const BONUS_ERROR_MSG = 'Нельзя потратить такое количество бонусов';
-    
+
     /**
      * @param \Bitrix\Main\Event $event
      */
@@ -40,7 +40,7 @@ class EventsHandlers
     {
         AddMessage2Log('OnBeforeSalePaymentSetFieldHandler work! ' . $event->getDebugInfo());
     }
-    
+
     /**
      * @return mixed
      */
@@ -48,7 +48,7 @@ class EventsHandlers
     {
         AddMessage2Log('OnBeforeEndBufferContentHandler work! ');
     }
-    
+
     /**
      * @param \Bitrix\Main\Event $event
      */
@@ -56,7 +56,7 @@ class EventsHandlers
     {
         AddMessage2Log('OnSaleOrderBeforeSavedHandler work! ' . $event->getDebugInfo());
     }
-    
+
     /**
      * @param \Bitrix\Main\Event $event
      */
@@ -64,7 +64,7 @@ class EventsHandlers
     {
         AddMessage2Log('OnSaleOrderPaidHandler work! ' . $event->getDebugInfo());
     }
-    
+
     /**
      * @param \Bitrix\Main\Event $event
      */
@@ -72,7 +72,7 @@ class EventsHandlers
     {
         AddMessage2Log('OnSaleStatusOrderChangeHandler work! ' . $event->getDebugInfo());
     }
-    
+
     /**
      * @param \Bitrix\Main\Event $event
      */
@@ -80,7 +80,7 @@ class EventsHandlers
     {
         AddMessage2Log('OnSaleOrderCanceledHandler work! ' . $event->getDebugInfo());
     }
-    
+
     /**
      * @param \Bitrix\Main\Event $event
      */
@@ -88,7 +88,7 @@ class EventsHandlers
     {
         AddMessage2Log('OnSaleOrderDeletedHandler work! ' . $event->getDebugInfo());
     }
-    
+
     /**
      * @param $arResult
      * @param $arUserResult
@@ -101,39 +101,7 @@ class EventsHandlers
         AddMessage2Log('OnSaleComponentOrderOneStepProcessHandler work! ' . $arUserResult . $arParams);
         return $arResult;
     }
-    
-    /**
-     * @param                          $order
-     * @param                          $arUserResult
-     * @param \Bitrix\Main\HttpRequest $request
-     * @param                          $arParams
-     * @param                          $arResult
-     */
-    public function OnSaleComponentOrderResultPreparedHandler($order, $arUserResult, HttpRequest $request, $arParams, &$arResult): void
-    {
-        if (ConfigProvider::getLoyaltyProgramStatus() === 'Y') {
-            $isBonusError     = false;
-            $bonusInput       = (int)$request->get('bonus-input');
-            $availableBonuses = (int)$request->get('available-bonuses');
-            
-            if ($bonusInput >= $availableBonuses) {
-                $arResult['LOYALTY']['ERROR'] = self::BONUS_ERROR_MSG;
-                $isBonusError                 = true;
-            }
-            
-            if (
-                $bonusInput > 0
-                && $availableBonuses > 0
-                && $isBonusError === false
-                && $arResult['JS_DATA']['TOTAL']['ORDER_TOTAL_PRICE'] >= $bonusInput
-            ) {
-                $arResult['JS_DATA']['TOTAL']['ORDER_TOTAL_PRICE']          -= $bonusInput;
-                $arResult['JS_DATA']['TOTAL']['ORDER_TOTAL_PRICE_FORMATED'] = number_format($arResult['JS_DATA']['TOTAL']['ORDER_TOTAL_PRICE'], 0, ',', ' ');
-                $arResult['JS_DATA']['TOTAL']['BONUS_PAYMENT']              = $bonusInput;
-            }
-        }
-    }
-    
+
     /**
      * Обработчик события, вызываемого ПОСЛЕ сохранения заказа
      *
@@ -144,7 +112,7 @@ class EventsHandlers
         /**@var \Bitrix\Sale\Order $order */
         $order = $event->getParameter("ENTITY");
         $isNew = $event->getParameter("IS_NEW");
-        
+
         if (isset($_POST['bonus-input'], $_POST['available-bonuses'])
             && $isNew
             && (int)$_POST['available-bonuses'] >= (int)$_POST['bonus-input']) {
@@ -152,19 +120,19 @@ class EventsHandlers
             $bonusCount = $_POST['bonus-input'];
             $service    = new LoyaltyService();
             $response   = $service->sendBonusPayment($orderId, $bonusCount);
-            
+
             //TODO - заглушка до появления api на стороне CRM. После появления реального апи - убрать
-            $response->success=true;
-            
+            $response->success = true;
+
             if ($response->success) {
                 try {
                     $bonusPaySystem    = PaySystemActionRepository::getFirstByWhere(['ID'], [['ACTION_FILE', '=', 'retailcrmbonus']]);
                     $paymentCollection = $order->getPaymentCollection();
-                    
+
                     if ($bonusPaySystem !== null) {
                         $service    = Manager::getObjectById($bonusPaySystem->getId());
                         $newPayment = $paymentCollection->createItem($service);
-                        
+
                         $newPayment->setField('SUM', $bonusCount);
                         $newPayment->setPaid('Y');
                         $order->save();
@@ -172,6 +140,31 @@ class EventsHandlers
                 } catch (ObjectPropertyException | ArgumentException | SystemException | Exception $e) {
                     AddMessage2Log('ERROR PaySystemActionRepository: ' . $e->getMessage());
                 }
+            }
+        }
+    }
+
+    public function OnSaleComponentOrderResultPreparedHandler($order, $arUserResult, HttpRequest $request, $arParams, &$arResult)
+    {
+        if (ConfigProvider::getLoyaltyProgramStatus() === 'Y') {
+            $isBonusError     = false;
+            $bonusInput       = (int)$request->get('bonus-input');
+            $availableBonuses = (int)$request->get('available-bonuses');
+
+            if ($bonusInput > $availableBonuses) {
+                $arResult['LOYALTY']['ERROR'] = self::BONUS_ERROR_MSG;
+                $isBonusError                 = true;
+            }
+
+            if (
+                $bonusInput > 0
+                && $availableBonuses > 0
+                && $isBonusError === false
+                && $arResult['JS_DATA']['TOTAL']['ORDER_TOTAL_PRICE'] > $bonusInput
+            ) {
+                $arResult['JS_DATA']['TOTAL']['ORDER_TOTAL_PRICE']          = $arResult['JS_DATA']['TOTAL']['ORDER_TOTAL_PRICE'] - $bonusInput;
+                $arResult['JS_DATA']['TOTAL']['ORDER_TOTAL_PRICE_FORMATED'] = number_format($arResult['JS_DATA']['TOTAL']['ORDER_TOTAL_PRICE'], 0, ',', ' ' );
+                $arResult['JS_DATA']['TOTAL']['BONUS_PAYMENT'] = $bonusInput;
             }
         }
     }
