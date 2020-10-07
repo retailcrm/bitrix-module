407358c7b73213a645079ab45f71dc01e2e6f95c
Sergey Chazov <oitv18@gmail.com>
2020-10-07 12:04:28 +0300

    add error message

--- intaro.retailcrm/install/export/local/components/intaro/sale.order.ajax/templates/.default/scripts/intaro.js
+++ intaro.retailcrm/install/export/local/components/intaro/sale.order.ajax/templates/.default/scripts/intaro.js
@@ -1,18 +1,31 @@
 $(document).ready(function() {
     let intervalId;
 
-    function make_ajax_request(e) {
+    function make_ajax_request() {
         setTimeout(function() {
+            $('#bonus-msg').html('Сколько бонусов потратить?');
             BX.Sale.OrderAjaxComponent.sendRequest();
-            console.log('test');
-        }, 2000);
+        }, 1005);
     }
 
     $('#bonus-input')
         .on('keydown', function() {
+            $('#bonus-msg').html('Обработка информации');
             clearInterval(intervalId);
         })
 
+    $('#bonus-input').keyup(function(){
+        let availableBonuses = Number.parseInt($('#available-bonus-input').val());
+        let inputBonuses = Number.parseInt($('#bonus-input').val());
+        if (inputBonuses > availableBonuses) {
+            $('#bonus-input-error').html('Вы не можете потратить более ' + availableBonuses + ' бонусов');
+        }else{
+            $('#bonus-input-error').html(null);
+        }
+
+
+    });
+
     $('#bonus-input').on('keydown',
-        _.debounce(make_ajax_request, 1300));
+        _.debounce(make_ajax_request, 1000));
 });
