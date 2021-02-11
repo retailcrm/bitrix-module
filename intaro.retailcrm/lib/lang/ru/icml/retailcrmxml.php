2f6023aac9a76ea3d7448d83eac3c098444913e8
Sergey Chazov <oitv18@gmail.com>
2021-02-11 20:58:00 +0300

    create separate classes for write and find data

---
+++ intaro.retailcrm/lib/lang/ru/icml/retailcrmxml.php
@@ -0,0 +1,12 @@
+<?php
+
+$MESS["ROOT_CATEGORY_FOR_CATALOG"] = "Корневой раздел для каталога \"%s\"";
+$MESS["PROPERTY_ARTICLE_HEADER_NAME"] = "Артикул";
+$MESS["PROPERTY_MANUFACTURER_HEADER_NAME"] = "Производитель";
+$MESS["PROPERTY_COLOR_HEADER_NAME"] = "Цвет";
+$MESS["PROPERTY_WEIGHT_HEADER_NAME"] = "Вес (габариты)";
+$MESS["PROPERTY_LENGTH_HEADER_NAME"] = "Длина (габариты)";
+$MESS["PROPERTY_WIDTH_HEADER_NAME"] = "Ширина (габариты)";
+$MESS["PROPERTY_HEIGHT_HEADER_NAME"] = "Высота (габариты)";
+$MESS["PROPERTY_PICTURE_HEADER_NAME"] = "Фото";
+$MESS["PROPERTY_SIZE_HEADER_NAME"] = "Размер";
