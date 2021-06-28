9fb0ec02af7cac5ad2221c95c093617cf59b7e8b
Sergey Chazov <oitv18@gmail.com>
2021-06-28 17:28:16 +0300

    bump version

--- intaro.retailcrm/install/version.php
+++ intaro.retailcrm/install/version.php
@@ -1,6 +1,6 @@
 <?php
 
 $arModuleVersion = [
-    'VERSION'      => '5.7.0',
-    'VERSION_DATE' => '2021-05-31 13:00:00',
+    'VERSION'      => '5.7.1',
+    'VERSION_DATE' => '2021-06-28 17:00:00',
 ];
