c6dd2973085aa342987414a3196b491c2c9a81ab
Ruslan Efanov <rus.efanov@bk.ru>
2021-08-22 22:07:57 +0300

    refactoring

--- intaro.retailcrm/classes/general/RCrmActions.php
+++ intaro.retailcrm/classes/general/RCrmActions.php
@@ -437,7 +437,7 @@ class RCrmActions
                 'active' => $active
             );
 
-            //self::apiMethod($api, 'marketplaceSettingsEdit', __METHOD__, $configuration);
+            self::apiMethod($api, 'marketplaceSettingsEdit', __METHOD__, $configuration);
         } else {
             $configuration = array(
                 'clientId' => $clientId,
@@ -450,7 +450,7 @@ class RCrmActions
                 'accountUrl' => $accountUrl
             );
 
-            //self::apiMethod($api, 'integrationModulesEdit', __METHOD__, $configuration);
+            self::apiMethod($api, 'integrationModulesEdit', __METHOD__, $configuration);
         }
     }
 
