bf51407551852ce1b434c15d2d6166bd4b4fcbb4
Neur0toxine <pashok9825@gmail.com>
2021-11-09 12:33:41 +0300

    bump to v5.8.3

--- intaro.retailcrm/install/version.php
+++ intaro.retailcrm/install/version.php
@@ -1,6 +1,6 @@
 <?php
 
 $arModuleVersion = [
-    'VERSION'      => '5.8.2',
-    'VERSION_DATE' => '2021-10-02 15:00:00',
+    'VERSION'      => '5.8.3',
+    'VERSION_DATE' => '2021-10-09 13:00:00',
 ];
