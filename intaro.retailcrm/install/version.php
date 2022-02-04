e3e0d41ff59f188d21b0853883b0358f01c6112a
Alex Lushpai <lushpai@gmail.com>
2022-02-04 15:16:14 +0300

    Backport (#242)
    * Add PRICE_DELIVERY for history exchange
    * Fix bug with LocationTable fqn
    * Fix bug with ProductType
    * Changed options for prices and corps
    * CustomersUpload: finding uniq clients by externalId
    * Restore shipment before deleting
    
    Co-authored-by: Sergey Chazov <oitv18@gmail.com>

--- intaro.retailcrm/install/version.php
+++ intaro.retailcrm/install/version.php
@@ -1,6 +1,6 @@
 <?php
 
 $arModuleVersion = [
-    'VERSION'      => '5.8.4',
-    'VERSION_DATE' => '2021-10-15 12:00:00',
+    'VERSION'      => '5.8.5',
+    'VERSION_DATE' => '2022-02-03 12:00:00',
 ];
