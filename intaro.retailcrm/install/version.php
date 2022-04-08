559bf79f7b8b88f518145f53d7e7614038bee9fa
Vlad <48670792+Mozgito@users.noreply.github.com>
2022-04-08 18:21:05 +0300

    Fix customized classes load (#245)
    * fix customized classes load
    * bump to 6.0.1

--- intaro.retailcrm/install/version.php
+++ intaro.retailcrm/install/version.php
@@ -1,6 +1,6 @@
 <?php
 
 $arModuleVersion = [
-    'VERSION'      => '6.0.0',
-    'VERSION_DATE' => '2022-02-04 13:00:00'
+    'VERSION'      => '6.0.1',
+    'VERSION_DATE' => '2022-04-08 17:00:00'
 ];
