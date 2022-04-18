dfad5edffbaff1d622d883b74f4f88950a8d5f7d
Alex Lushpai <lushpai@gmail.com>
2022-04-18 16:49:59 +0300

    update to 6.0.2

--- intaro.retailcrm/install/version.php
+++ intaro.retailcrm/install/version.php
@@ -1,6 +1,6 @@
 <?php
 
 $arModuleVersion = [
-    'VERSION'      => '6.0.1',
-    'VERSION_DATE' => '2022-04-08 17:00:00'
+    'VERSION'      => '6.0.2',
+    'VERSION_DATE' => '2022-04-18 17:00:00'
 ];
