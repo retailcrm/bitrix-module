e0efae4618db0dcb7b24d38f2564052ede699a67
Mozgito <mozgito@gmail.com>
2022-06-14 12:17:46 +0300

    update to 6.0.5

--- intaro.retailcrm/install/version.php
+++ intaro.retailcrm/install/version.php
@@ -1,6 +1,6 @@
 <?php
 
 $arModuleVersion = [
-    'VERSION'      => '6.0.4',
-    'VERSION_DATE' => '2022-05-18 17:00:00'
+    'VERSION'      => '6.0.5',
+    'VERSION_DATE' => '2022-06-14 12:00:00'
 ];
