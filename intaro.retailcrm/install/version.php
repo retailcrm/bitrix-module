26639fd5d6dfd358475bc5b458828cdb1a8c9e77
KMityai <dkruglovn@gmail.com>
2022-11-22 10:41:35 +0300

    bump to 6.1.8

--- intaro.retailcrm/install/version.php
+++ intaro.retailcrm/install/version.php
@@ -1,6 +1,6 @@
 <?php
 
 $arModuleVersion = [
-    'VERSION'      => '6.1.7',
-    'VERSION_DATE' => '2022-09-09 15:00:00'
+    'VERSION'      => '6.1.8',
+    'VERSION_DATE' => '2022-09-30 14:00:00'
 ];
