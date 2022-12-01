98a3dae825000587a95b07e9d342e7f942b262ad
Ivan Chaplygin <chaplygin@retailcrm.ru>
2022-12-01 11:46:21 +0300

    fix reg empty client email, sending a comment the order status

--- intaro.retailcrm/install/version.php
+++ intaro.retailcrm/install/version.php
@@ -1,6 +1,6 @@
 <?php
 
 $arModuleVersion = [
-    'VERSION'      => '6.1.8',
-    'VERSION_DATE' => '2022-09-30 14:00:00'
+    'VERSION'      => '6.1.9',
+    'VERSION_DATE' => '2022-12-01 11:30:00'
 ];
