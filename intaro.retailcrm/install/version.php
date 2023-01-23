1c7b09cf5ee5ea4da958c8ef9c3a5f755dd968ea
Максим Кузнецов <kuznetsov@retailcrm.ru>
2023-01-23 11:15:04 +0300

    ref #75292 Bump to 6.1.14

--- intaro.retailcrm/install/version.php
+++ intaro.retailcrm/install/version.php
@@ -1,6 +1,6 @@
 <?php
 
 $arModuleVersion = [
-    'VERSION'      => '6.1.13',
-    'VERSION_DATE' => '2023-01-10 17:00:00'
+    'VERSION'      => '6.1.14',
+    'VERSION_DATE' => '2023-01-20 17:00:00'
 ];
