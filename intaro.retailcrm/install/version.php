be1447c6ec503ce8d8fbff0ece511dbf1f203566
Kocmonavtik <61938582+Kocmonavtik@users.noreply.github.com>
2023-04-11 12:20:53 +0300

    ref #89247 Добавление валидации на тип свойства в экспорте каталога  (#285)

--- intaro.retailcrm/install/version.php
+++ intaro.retailcrm/install/version.php
@@ -1,6 +1,6 @@
 <?php
 
 $arModuleVersion = [
-    'VERSION'      => '6.3.0',
-    'VERSION_DATE' => '2023-04-06 17:00:00'
+    'VERSION'      => '6.3.1',
+    'VERSION_DATE' => '2023-04-10 10:00:00'
 ];
