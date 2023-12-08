7a31e91b0ebcd5c35e7535303beb354791b8a4e9
Uryvskiy Dima <uryvskiydima@gmail.com>
2023-12-08 12:19:41 +0300

    Исправлена ошибка при передаче подписки на рекламно-информационные рассылки

--- intaro.retailcrm/install/version.php
+++ intaro.retailcrm/install/version.php
@@ -1,6 +1,6 @@
 <?php
 
 $arModuleVersion = [
-    'VERSION'      => '6.4.10',
-    'VERSION_DATE' => '2023-10-25 15:00:00'
+    'VERSION'      => '6.4.11',
+    'VERSION_DATE' => '2023-12-08 13:00:00'
 ];
