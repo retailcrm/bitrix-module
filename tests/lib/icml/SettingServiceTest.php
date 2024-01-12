57556bf14db2af19d1b187e17238f206a10530a7
Ivan Chaplygin <chaplygin@retailcrm.ru>
2024-01-12 16:37:03 +0300

    ref #93387 Добавление тестов

---
+++ tests/lib/icml/SettingServiceTest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace lib\icml;
+
+use Intaro\RetailCrm\Icml\SettingsService;
+
+class SettingServiceTest extends \BitrixTestCase
+{
+    private $mockSettingService;
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->mockSettingService = $this->getMockBuilder(SettingsService::class)
+            ->disableOriginalConstructor()
+            ->getMock()
+        ;
+    }
+
+    public function testConstruct(): SettingsService
+    {
+        $path = $_SERVER['DOCUMENT_ROOT'] . '/local/';
+
+        CheckDirPath($path);
+
+        $file = new \Bitrix\Main\IO\File($path . '/addProperty.txt', $siteId = null);
+
+        $file->putContents("property1 = test prop \n property2 = test prop 2");
+
+        $settingService = SettingsService::getInstance($this->getSetupVars(), "");
+
+        $this->assertInstanceOf(SettingsService::class, $settingService);
+        $this->assertArrayHasKey('property1', $settingService->actrualPropList);
+        $this->assertArrayHasKey('property2', $settingService->actrualPropList);
+
+        return $settingService;
+    }
+
+    private function getSetupVars()
+    {
+        return [
+            'iblockExport' => 2,
+            'loadPurchasePrice' => "",
+            'loadNonActivity' => "",
+            'SETUP_FILE_NAME' => "/bitrix/catalog_export/retailcrm.xml",
+            'SETUP_PROFILE_NAME' => "Выгрузка каталога RetailCRM"
+        ];
+    }
+}
