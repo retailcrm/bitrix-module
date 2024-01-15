c5446ffda261a612723f9d2f306ad5cc2c15fdfb
Ivan Chaplygin <chaplygin@retailcrm.ru>
2024-01-15 12:21:30 +0300

    ref #92570 Удаление закоментированного кода

--- intaro.retailcrm/lang/en/install/step1.php
+++ intaro.retailcrm/lang/en/install/step1.php
@@ -13,7 +13,6 @@ $MESS ['ERR_FIELDS_API_HOST'] = 'Fields are filled incorrectly.';
 $MESS ['ERR_METHOD_NOT_FOUND'] = 'Check availability of API methods for current key.';
 $MESS ['ERR_EMPTY_SITES_LIST'] = 'At least 1 store must be matched';
 $MESS ['ERR_GET_SITE_CRM'] = 'Failed to retrieve CRM store';
-//$MESS ['URL_NOT_FOUND'] = 'One or more sites have empty "server URL" field.';
 $MESS ['INFO_1'] = 'Enter the address of RetailCRM instance (for example, https://demo.retailcrm.ru) and API key.';
 $MESS ['INFO_2'] = 'API key can be generated when the store is registered in RetailCRM (Administration > Integration).';
 $MESS ['INFO_3'] = 'Store code in 1C-Bitrix must correspond with the store code in RetailCRM (Administration > Stores).';
