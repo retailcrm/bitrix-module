85749c8602855f1ab7af6cd90483c3f60e5211d5
Kocmonavtik <61938582+Kocmonavtik@users.noreply.github.com>
2024-03-18 14:25:20 +0300

    Обновление версии модуля (#340)

--- intaro.retailcrm/install/version.php
+++ intaro.retailcrm/install/version.php
@@ -1,6 +1,6 @@
 <?php
 
 $arModuleVersion = [
-    'VERSION'      => '6.5.9',
-    'VERSION_DATE' => '2024-03-01 12:30:00'
+    'VERSION'      => '6.5.10',
+    'VERSION_DATE' => '2024-03-18 14:30:00'
 ];
