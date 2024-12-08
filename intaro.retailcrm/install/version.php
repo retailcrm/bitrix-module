901bd37144e291e6cc4886db91b3ed4218295432
Aleksei Novikov <drnovikov@gmail.com>
2024-12-08 21:44:19 +0600

    fix: fix breaking orders made with Maxma promocodes
    Prevent breaking orders caused by undocumented behavior of Bitrix\Sale\Internals\Fields objects upon modification of their PRICE
    
    Refs: https://github.com/retailcrm/bitrix-module/issues/374

--- intaro.retailcrm/install/version.php
+++ intaro.retailcrm/install/version.php
@@ -1,6 +1,6 @@
 <?php
 
 $arModuleVersion = [
-    'VERSION'      => '6.5.38',
-    'VERSION_DATE' => '2024-10-31 15:00:00'
+    'VERSION'      => '6.5.39',
+    'VERSION_DATE' => '2024-12-08 18:30:00'
 ];
