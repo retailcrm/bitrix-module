84c7d10146363e8b75cb0191522b1393061d44c3
Kocmonavtik <61938582+Kocmonavtik@users.noreply.github.com>
2025-01-15 10:24:03 +0300

    Исправлены ошибки при обновлении модуля (#379)

--- intaro.retailcrm/install/version.php
+++ intaro.retailcrm/install/version.php
@@ -1,6 +1,6 @@
 <?php
 
 $arModuleVersion = [
-    'VERSION'      => '6.6.2',
-    'VERSION_DATE' => '2025-01-14 13:00:00'
+    'VERSION'      => '6.6.3',
+    'VERSION_DATE' => '2025-01-14 18:00:00'
 ];
