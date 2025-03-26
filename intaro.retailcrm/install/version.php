0359315c792c1762ee7c05259abe4bef121a14aa
ellynoize <111681973+ellynoize@users.noreply.github.com>
2025-03-26 15:45:25 +0300

    Исправлена передача некорректного статуса оплаты для отмененных заказов (#387)

--- intaro.retailcrm/install/version.php
+++ intaro.retailcrm/install/version.php
@@ -1,6 +1,6 @@
 <?php
 
 $arModuleVersion = [
-    'VERSION'      => '6.6.9',
-    'VERSION_DATE' => '2025-03-04 16:00:00'
+    'VERSION'      => '6.6.10',
+    'VERSION_DATE' => '2025-03-25 16:00:00'
 ];
