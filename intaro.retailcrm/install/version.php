1a49facea1acc85a64d3039b849812a89da4add6
ellynoize <ellynoize@kali.ellynoize>
2025-05-12 15:55:42 +0300

    rebase

--- intaro.retailcrm/install/version.php
+++ intaro.retailcrm/install/version.php
@@ -1,6 +1,6 @@
 <?php
 
 $arModuleVersion = [
-    'VERSION'      => '6.6.12',
-    'VERSION_DATE' => '2025-05-06 16:00:00'
+    'VERSION'      => '6.6.13',
+    'VERSION_DATE' => '2025-05-12 16:00:00'
 ];
