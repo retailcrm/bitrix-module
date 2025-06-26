abbff2c3fce050afce7cc1802f13ee87234fd81b
Ivan Chaplygin <chaplygin@retailcrm.ru>
2025-06-26 15:01:27 +0300

    Обновление версии модуля

--- intaro.retailcrm/install/version.php
+++ intaro.retailcrm/install/version.php
@@ -1,6 +1,6 @@
 <?php
 
 $arModuleVersion = [
-    'VERSION'      => '6.6.19',
-    'VERSION_DATE' => '2025-06-19 12:30:00'
+    'VERSION'      => '6.6.20',
+    'VERSION_DATE' => '2025-06-26 15:30:00'
 ];
