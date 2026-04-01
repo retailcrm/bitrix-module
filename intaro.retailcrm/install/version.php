537cec1c5b4920ef5a551dbcecc0c75284bc420f
danila <kobelev@retailcrm.ru>
2026-04-01 19:47:00 +0500

    Исправлена ошибка переноса данных между клиентами при синхронизации истории

--- intaro.retailcrm/install/version.php
+++ intaro.retailcrm/install/version.php
@@ -1,6 +1,6 @@
 <?php
 
 $arModuleVersion = [
-    'VERSION'      => '6.7.9',
-    'VERSION_DATE' => '2026-03-16 17:30:00'
+    'VERSION'      => '6.7.10',
+    'VERSION_DATE' => '2026-04-01 17:30:00'
 ];
