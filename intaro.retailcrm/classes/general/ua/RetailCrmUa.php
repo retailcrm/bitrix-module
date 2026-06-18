2fe6225a2909ee29e0ad8bf5135fbd1ae8d01285
ellynoize <na1ton.trik@gmail.com>
2026-06-18 14:24:16 +0300

    fixes

--- intaro.retailcrm/classes/general/ua/RetailCrmUa.php
+++ intaro.retailcrm/classes/general/ua/RetailCrmUa.php
@@ -20,6 +20,7 @@ class RetailCrmUa
     public static $MODULE_ID = 'intaro.retailcrm';
     public static $CRM_UA = 'ua';
     public static $CRM_UA_KEYS = 'ua_keys';
+    private const JSON_FLAGS = JSON_HEX_TAG | JSON_HEX_APOS | JSON_HEX_AMP | JSON_HEX_QUOT;
     
     public static function add()
     {
@@ -32,6 +33,12 @@ class RetailCrmUa
 
         if ($ua === 'Y' && !empty($uaKeys[SITE_ID]['ID']) && !empty($uaKeys[SITE_ID]['INDEX']) && $request->isAdminSection() !== true) {
             global $APPLICATION;
+            $trackerId = json_encode((string) $uaKeys[SITE_ID]['ID'], self::JSON_FLAGS);
+            $dimensionIndex = preg_replace('/\D+/', '', (string) $uaKeys[SITE_ID]['INDEX']);
+
+            if ($dimensionIndex === '') {
+                return;
+            }
 
             $ua = "
             <script type=\"text/javascript\">
@@ -39,14 +46,14 @@ class RetailCrmUa
                 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
                 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
                 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-                ga('create', '" . $uaKeys[SITE_ID]['ID'] . "', 'auto');
+                ga('create', " . $trackerId . ", 'auto');
                 function getRetailCRMCookie(name) {
                       var matches = document.cookie.match(new RegExp(
                           '(?:^|; )' + name + '=([^;]*)'
                       ));
                       return matches ? decodeURIComponent(matches[1]) : '';
                 }
-                ga('set', 'dimension" . $uaKeys[SITE_ID]['INDEX'] . "', getRetailCRMCookie('_ga'));
+                ga('set', 'dimension" . $dimensionIndex . "', getRetailCRMCookie('_ga'));
                 ga('send', 'pageview');
             </script>";
 
@@ -65,14 +72,18 @@ class RetailCrmUa
                         'PRICE' => $order->getPrice(),
                         'DISCOUNT_VALUE' => $order->getField('DISCOUNT_VALUE')
                     );
+                    $safeOrderId = json_encode((string) $arOrder['ID'], self::JSON_FLAGS);
+                    $safeServerName = json_encode((string) ($_SERVER['SERVER_NAME'] ?? ''), self::JSON_FLAGS);
+                    $safePrice = json_encode((float) $arOrder['PRICE'], self::JSON_FLAGS);
+                    $safeDiscountValue = json_encode((float) $arOrder['DISCOUNT_VALUE'], self::JSON_FLAGS);
 
                     $ua .= "<script type=\"text/javascript\">
                     ga('require', 'ecommerce', 'ecommerce.js');
                     ga('ecommerce:addTransaction', {
-                      'id': $arOrder[ID],
-                      'affiliation': '$_SERVER[SERVER_NAME]', 
-                      'revenue': $arOrder[PRICE],
-                      'tax': $arOrder[DISCOUNT_VALUE]
+                      'id': " . $safeOrderId . ",
+                      'affiliation': " . $safeServerName . ", 
+                      'revenue': " . $safePrice . ",
+                      'tax': " . $safeDiscountValue . "
                     });
                     ";
                     $arBasketItems = CsaleBasket::GetList(
@@ -83,13 +94,18 @@ class RetailCrmUa
                         array('PRODUCT_ID', 'NAME', 'PRICE', 'QUANTITY', 'ORDER_ID', 'ID')
                     );  
                     while ($arItem = $arBasketItems->fetch()) {
+                        $safeProductId = json_encode((string) $arItem['PRODUCT_ID'], self::JSON_FLAGS);
+                        $safeProductName = json_encode((string) $arItem['NAME'], self::JSON_FLAGS);
+                        $safeItemPrice = json_encode((float) $arItem['PRICE'], self::JSON_FLAGS);
+                        $safeQuantity = json_encode((float) $arItem['QUANTITY'], self::JSON_FLAGS);
+
                         $ua .= "
                         ga('ecommerce:addItem', {
-                            'id': $arOrder[ID],
-                            'sku': '$arItem[PRODUCT_ID]',
-                            'name': '$arItem[NAME]',
-                            'price': $arItem[PRICE],
-                            'quantity': $arItem[QUANTITY]
+                            'id': " . $safeOrderId . ",
+                            'sku': " . $safeProductId . ",
+                            'name': " . $safeProductName . ",
+                            'price': " . $safeItemPrice . ",
+                            'quantity': " . $safeQuantity . "
                         });
                         ";
                     }
